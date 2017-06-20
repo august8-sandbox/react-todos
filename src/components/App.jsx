2bb357a73e6efdde9216a95d9bd957405214f898
August8 <hazuki@people.co.jp>
2017-06-20 17:20:49 +0900

    1st commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// export default class Game extends React.Component {
+//   render() {
+//     return (
+//       <div>Hello World!!</div>
+//     );
+//   }
+// }
+
+// const App = () => <div>Hello World!!!</div>;
+// export default App;
+
+function App() {
+  return (
+    <div>Hello Wolrd!!!!</div>
+  );
+}
+
+export default App;
---
+++ webpack.config.babel.js
@@ -0,0 +1,63 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import webpack from 'webpack';
+import path from 'path';
+import packageInfo from './package.json';
+
+export default {
+  entry: [
+    path.join(__dirname, 'src/main'),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules',
+    ],
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
+        PACKAGE_VERSION: JSON.stringify(packageInfo.version),
+      },
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    compress: true,
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    hot: true,
+  },
+};
